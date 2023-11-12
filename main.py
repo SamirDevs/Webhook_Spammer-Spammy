b9854feda1b8fe8ba74481bc3aba4d934c65d680
vvs <150073571+SamirDevs@users.noreply.github.com>
2023-11-11 22:06:58 -0400

    Add files via upload
    main files

---
+++ main.py
@@ -0,0 +1,20 @@
+import time, requests, pyfiglet, threading
+print(pyfiglet.figlet_format("Spammy!"))
+
+msg = input("Please Insert Webhook Spam Message: ")
+webhook = input("Please Insert WebHook URL: ")
+th = int(input('Number of thread ? (200 recommended): '))
+sleep = int(input("Sleeping time ? (recommended 2): "))
+def spam():
+    while True:
+        try:
+            data = requests.post(webhook, json={'content': msg})
+            if data.status_code == 204:
+                print(f"Sent MSG {msg}")
+        except:
+            print("Bad Webhook :" + webhook)
+        time.sleep(sleep)
+    
+for x in range(th):
+    t = threading.Thread(target = spam)
+    t.start()
